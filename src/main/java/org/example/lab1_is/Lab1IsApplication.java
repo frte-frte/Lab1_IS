9a31fa3a5a79b06f2d2be35e81fc66a4e86a332f
frte--frte <demasamson04@gmail.com>
2024-11-09 13:35:44 +0300

    add base

---
+++ src/main/java/org/example/lab1_is/Lab1IsApplication.java
@@ -0,0 +1,14 @@
+package org.example.lab1_is;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1IsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1IsApplication.class, args);
+
+    }
+
+}
